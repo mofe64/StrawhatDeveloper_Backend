fb4b1eea89084533b4028b0c4cdadfadd232797d
Eyimofe Ogunbiyi <ogunbiyioladapo33@gmail.com>
2021-05-03 10:11:16 +0100

    setup main branch

---
+++ src/main/java/com/strawhatdev/backend/StrawhatdevBackendApplication.java
@@ -0,0 +1,13 @@
+package com.strawhatdev.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrawhatdevBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrawhatdevBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/strawhatdev/backend/StrawhatdevBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.strawhatdev.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrawhatdevBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
